0317b94b7bca89da3d60cb20b437a11cf61733ba
saidur <saidurrahman179@gmail.com>
2022-03-17 11:35:58 +0600

    after won work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Display from "./component/Display";
+
 
 function App() {
+  const [count,setCount] = useState(0)
+  const todo = [
+    {id:"1",title:"This is my fast todo",author:"saidur rahman"},
+    {id:"2",title:"This is my fast todo",author:"asadur"},
+    {id:"3",title:"This is my fast todo",author:"saiful"},
+    {id:"4",title:"This is my fast todo",author:"shorif"},
+    {id:"5",title:"This is my fast todo",author:"Habibour rahman"},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <h1>Count value is = {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={()=>setCount(count-1)}>Increment</button>
+     <Display></Display>
+      <Show></Show>
+      
+      {todo.map(single=><Singlepost key={single.id}   data ={single}></Singlepost>)}
     </div>
   );
 }
 
+
 export default App;
+function Singlepost(props){
+  const {title,author,id} = props?.data
+  console.log(props)
+return(
+  <div id={id}>
+    <h1> This is title {title}</h1>
+    <p>{author}</p>
+  </div>
+)
+}
+
+function Show(){
+  return(
+    <div>Hello, This is show Component running !</div>
+  )
+}
---
+++ src/component/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Display extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This is Display Component </h1>
+            </div>
+        )
+    }
+}
+
+export default Display;
